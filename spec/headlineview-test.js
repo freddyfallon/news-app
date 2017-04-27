0fed8fa31ac182ef7bf0c626f032ad95128d4e93
Freddy Fallon <freddyfallon@gmail.com>
2017-04-27 15:44:08 +0100

    Acess API

--- spec/headlineview-test.js
+++ spec/headlineview-test.js
@@ -1,3 +1,8 @@
 function HeadlineViewTest() {
+  var headlines = ["Hello", "Bro"]
+  var headlineView = new HeadlineView(headlines);
 
+  assert.ToEqual(headlineView.viewHeadlines(), "<ul><li><div>Hello</div></li><li><div>Bro</div></li></ul>")
 };
+
+HeadlineViewTest();
